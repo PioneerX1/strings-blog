2cea561fe36a7d7cf4b7b7ca5af867ec9fd09ac9
Mick Sexton <lacrookedbeat@hotmail.com>
2017-08-10 12:30:44 -0700

    add delete blogpost functionality

--- app/components/blogpost-detail.js
+++ app/components/blogpost-detail.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    deletePost(blogpost) {
+      if (confirm('Are You Sure You Want to Delete this Post?')) {
+        this.sendAction('destroyPost', blogpost);
+      }
+    }
+  }
 });
